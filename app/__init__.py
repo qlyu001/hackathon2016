884c7c3692b1f1de2c53fbc1d2f7e48eb017df49
ksemelka <kylesemelka@gmail.com>
2017-02-11 18:00:18 -0800

    Reverted code to get it working

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config.from_object('app.config')
-app.config.from_envvar('YOURAPPLICATION_SETTINGS')
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+#app.config.from_object('app.config')
+#app.config.from_envvar('YOURAPPLICATION_SETTINGS')
+#db = SQLAlchemy(app)
+#login_manager = LoginManager()
+#login_manager.init_app(app)
 
 from app import views, models
--- app/config.py
+++ app/config.py
@@ -1,12 +1,12 @@
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+#WTF_CSRF_ENABLED = True
+#SECRET_KEY = 'you-will-never-guess'
 
-OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'}]
+#OPENID_PROVIDERS = [
+ #   {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+  #  {'name': 'Yahoo', 'url': 'https://me.yahoo.com'}]
 
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+#import os
+#basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
